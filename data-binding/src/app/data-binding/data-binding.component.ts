38fdbb19e9645b6f0228dea6a62ddeab4fd5a753
Pier Branco <pier.branco@bit.com.br>
2019-09-10 14:07:29 -0300

    Aula 11 - Event binding. Formas diferentes de associar um event na tag do html associado a uma função na logica. Uso de # para identificar o event target de um input

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,21 +3,47 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.scss']
+  // styleUrls: ['./data-binding.component.scss']
+  styles:[`.highlight{
+              font-weight: bold;
+              background-color: yellow;
+          }`]
 })
 export class DataBindingComponent implements OnInit {
   url: string = "http://loiane.com";
   cursoAngular: boolean = true;
-  urlImagem: string = "http://lorempixel.com/400/200/"
+  urlImagem: string = "http://lorempixel.com/400/200/";
+  valorAtual: String = ""
+  valorSalvo: string = ""
+
+  isMouseOver: boolean = false
 
 
   getCurtirCurso(){
     return true
   }
 
+  botaoClicado(){
+    alert("Botão clicado")
+  }
+
+  onKeyUp(e: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>e.target).value
+
+  }
+
 
   getValor(){
     return 1
+  }
+
+  salvarValor(valor){
+    this.valorSalvo=valor
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver
+    console.log(this.isMouseOver)
   }
 
   constructor() { }
